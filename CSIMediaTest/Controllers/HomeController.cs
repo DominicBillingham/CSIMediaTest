d9c664f9c348f7d9fafa48c7132027e8de9b7cbc
Dominic Billingham <dominicbillingham3232@gmail.com>
2023-11-13 20:27:18 +0000

    Add project files.

---
+++ CSIMediaTest/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using CSIMediaTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CSIMediaTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sort(string numberList, bool sortByAscending)
+        {
+
+            List<int> numberArray = new List<int>();
+            foreach (string number in numberList.Split(" "))
+            {
+                numberArray.Add(int.Parse(number));
+            }
+
+
+            return Ok(numberArray);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
